ae2bcacade8eb8d124e07fcd5dca1d55a76a8646
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-08 20:25:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ def take_a_number(katz_deli,name)
 puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 end
-def now_serving(katz_deli,name)
+def now_serving(katz_deli)
   if katz_deli.length < 1
       puts "There is nobody waiting to be served!"
     else katz_deli.length > 1
