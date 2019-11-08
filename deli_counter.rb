3c406d6d53c56a903519d70d6a3cc9d41ba3fadc
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-08 20:02:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,7 @@ def take_a_number(katz_deli,name)
     katz_deli << name
     puts katz_deli[katz_deli.length-1]
   else katz_deli.length >= 1
+    binding.pry
         puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
   end
 end
