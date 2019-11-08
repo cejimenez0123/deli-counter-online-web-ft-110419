e6f05633f42402bb4c61746c54673b7713ba0f7a
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-08 19:45:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,18 +4,20 @@ def line(katz_deli)
   if katz_deli.length < 1
     puts "The line is currently empty."
   else katz_deli.length >= 1
-<<<<<<< HEAD
+
     statement = "The line is currently: "
     katz_deli.each.with_index(1) do |customer,index|
    number = "#{index}. #{customer} "
    statement << number
+ end
    puts statement
-=======
+end
+end
     statement = "The line is currently:"
     katz_deli.each.with_index(1) do |customer,index|
    y = "#{index}. #{customer}"
   puts statement.concat(y)
->>>>>>> 09dcf650f19143ef50120132c05042bac1783912
+
 end
 end
   end
