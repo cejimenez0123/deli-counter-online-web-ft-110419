6288b6f467deb3404df47f58c2196a2641ecf39a
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-08 20:00:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(katz_deli)
 end
 end
 def take_a_number(katz_deli,name)
-  if katz_deli.length == 0
+  if katz_deli.length < 1
     katz_deli << name
     puts katz_deli.join
   else katz_deli.length >= 1
