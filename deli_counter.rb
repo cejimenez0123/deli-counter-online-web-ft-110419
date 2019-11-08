99af80f65eaa72531699b66a63e68371a8543e43
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-08 19:56:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,7 @@ end
 end
 def take_a_number(katz_deli,name)
   if katz_deli.length == 0
+    katz_deli << name
     puts katz_deli
   else katz_deli.length >= 1
   katz_deli.each.with_index(1) do |customer,index|
